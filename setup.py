faac24b61e63e5c797efdcac3f84824b390368fe
Sascha Thiede <sascha.thiede@outlook.de>
2023-06-24 15:14:52 +0200

    setup stuff

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='oellrich_graph',
+    version='1.0.0',
+    description='Python implementation of a generic graph class, derived from the C++ implementation of Prof. Martin Oellrich',
+    author='Sascha {Thiede, Kohler}',
+)
