762e97ac3d72cb87148355c461094fb96eaeb4f9
Sascha Thiede <sascha.thiede@outlook.de>
2023-06-24 15:53:43 +0200

    fix naming order

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='oellrich_graph',
     version='1.0.0',
     description='Python implementation of a generic graph class, derived from the C++ implementation of Prof. Martin Oellrich',
-    author='Sascha {Thiede, Kohler}',
+    author='Sascha {Kohler, Thiede}',
     packages=['oellrich_graph'],
 )
