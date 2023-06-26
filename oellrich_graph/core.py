120d6351b3409621a418bd961361846569ac1e64
Sascha Thiede <sascha.thiede@outlook.de>
2023-06-26 15:18:05 +0200

    modify splitting for edge string

--- oellrich_graph/core.py
+++ oellrich_graph/core.py
@@ -136,7 +136,7 @@ class Edge:
                     f"Edge: load_from_string() parameter {param} is already set!"
                 )
         # split the string into its components
-        components = string.split(" ")
+        components = string.split()
         # check if the string has the correct format
         if len(components) > 4 and not components[4].startswith("#"):
             raise Warning(
