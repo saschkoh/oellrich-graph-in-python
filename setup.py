8c19728275e886a68ce863eb28f7bf8b815807a8
Sascha Thiede <sascha.thiede@outlook.de>
2023-06-27 10:33:15 +0200

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='oellrich_graph',
-    version='2.2.0',
+    version='2.3.0',
     description='Python implementation of a generic graph class, derived from the C++ implementation of Prof. Martin Oellrich',
     author='Sascha {Kohler, Thiede}',
     packages=['oellrich_graph'],
